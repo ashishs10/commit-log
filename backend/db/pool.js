87ce784609e3f83e11ddcdd5ce9d0707875229bd
ashishs10 <ashish9six@gmail.com>
2026-04-14 00:31:07 +0530

    express server Scaffolding

---
+++ backend/db/pool.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+export default pool;
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express from "express";
+import pool from "./db/pool.js";
+
+const app = express();
+
+app.use(express.json());
+
+// db connection
+const testDb = async () => {
+  try {
+    const response = await pool.query(`SELECT NOW()`);
+    console.log("DB Connected ", response.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+testDb();
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    ok: true,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server started at port ", process.env.PORT);
+});
